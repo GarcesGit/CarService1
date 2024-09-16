b2ed88e3042710372d9f414e9f4f7d85c1221ddd
Natalya Koshukova <koshukovanat@gmail.com>
2024-09-16 13:31:39 +0700

    .

---
+++ SibirFRANService/scripts/javascripts.js
@@ -0,0 +1,30 @@
+function animaionItem(i) {
+    let itemsArray = document.getElementsByClassName("service-item-name");
+    let item = itemsArray[i - 1];
+    let itemWrapArr = document.getElementsByClassName("service-item");
+    let itemWrap = itemWrapArr[i - 1];
+
+    if (item.classList.contains("showNote")) {
+        function removeText() {
+            (function () {
+                document.getElementById("service-note" + i).style.visibility = "hidden";
+                document.getElementById("service-note" + i).style.left = "300px";
+                document.getElementById("service-note" + i).style.transition = "1s";
+            })();
+            item.classList.remove("showNote");
+            itemWrap.style.padding = "80px 0 0 0";
+            itemWrap.style.transition = "1s";
+        }
+        setTimeout(removeText, 300);
+    } else {
+        item.classList.add("showNote");
+        itemWrap.style.padding = "0px";
+        itemWrap.style.transition = "1s";
+        function text() {
+            document.getElementById("service-note" + i).style.visibility = "visible";
+            document.getElementById("service-note" + i).style.left = "0px";
+            document.getElementById("service-note" + i).style.transition = "1s";
+        }
+        setTimeout(text, 300);
+    }
+}
